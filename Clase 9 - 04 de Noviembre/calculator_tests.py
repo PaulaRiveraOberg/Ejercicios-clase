0f3ec5fb3ac5bd09e88b4b5b19f07398b5180572
PaulaRiveraOberg <priveraoberg@gmail.com>
2024-11-04 22:02:02 -0300

    Update calculator_tests.py

--- Clase 9 - 04 de Noviembre/calculator_tests.py
+++ Clase 9 - 04 de Noviembre/calculator_tests.py
@@ -8,6 +8,11 @@ class TestSimpleCalculator(unittest.TestCase):
         cls.calculator = Calculator()
         print("Hola soy setUpClass")
 
+    #Devuelve la base original limpia
+    @classmethod
+    def tearDownClass(cls):
+        print("Limpieza después de todos los tests")
+
     def setUp(self):
         print("Hola soy setUp")
 
@@ -20,6 +25,14 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_division_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             self.calculator.divide(2, 0)
+
+    def test_division(self):
+        self.assertEqual(self.calculator.divide(4, 2), 2)
+        self.assertEqual(self.calculator.divide(-4, 2), -2)
+        self.assertEqual(self.calculator.divide(0, 1), 0)
+        # Prueba de excepción esperada al dividir por cero
+        with self.assertRaises(ZeroDivisionError):
+            self.calculator.divide(1, 0)
 
 
 if __name__ == '__main__':
